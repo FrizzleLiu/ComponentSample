3e7a5f20f03ea1e55558531d21fdff5d0ea3c9cb
sean <15888897624@163.com>
2020-08-28 20:50:00 +0800

    基础模块创建gradle配置

---
+++ order/src/main/java/com/frizzle/modular/order/Order_MainActivity.java
@@ -0,0 +1,15 @@
+package com.frizzle.modular.order;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class Order_MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.order_activity_main);
+    }
+}
---
+++ order/src/main/java/com/frizzle/modular/order/debug/Order_DebugActivity.java
@@ -0,0 +1,16 @@
+package com.frizzle.modular.order.debug;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.frizzle.modular.order.R;
+
+public class Order_DebugActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.order_activity_debug);
+    }
+}
